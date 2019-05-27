67726fda549bacd08eb55ce970f015b16dc74fc9
nicobidone <nicobidone@gmail.com>
2019-05-26 23:22:16 -0300

    First commit

--- app/src/main/java/com/unicen/exa/ingenieria/MainActivity.java
+++ app/src/main/java/com/unicen/exa/ingenieria/MainActivity.java
@@ -2,6 +2,8 @@ package com.unicen.exa.ingenieria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         presenter = new MainPresenter(new MainView(this), new MainModel());
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        //myWebView.loadUrl("https://www.nintenderos.com/");
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        myWebView.loadUrl("file:///android_asset/test.html");
+
+        //myWebView.loadData(customHTML,"text/html","UTF-8");
     }
 }
---
+++ app/src/main/java/com/unicen/exa/ingenieria/WebAppInterface.java
@@ -0,0 +1,48 @@
+package com.unicen.exa.ingenieria;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class WebAppInterface {
+    Context mContext;
+
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public String getInts(){
+        Integer[] aux = new Integer[3];
+        aux[0] = 100;
+        aux[1] = 200;
+        aux[2] = 300;
+        return a1dToJson(aux).toString();
+    }
+
+    @JavascriptInterface
+    public Integer getInt(){
+        return 100;
+    }
+
+    private String a1dToJson(Integer[] data) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("[");
+        for (int i = 0; i < data.length; i++) {
+            Integer d = data[i];
+            if (i > 0)
+                sb.append(",");
+            sb.append(d);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
+

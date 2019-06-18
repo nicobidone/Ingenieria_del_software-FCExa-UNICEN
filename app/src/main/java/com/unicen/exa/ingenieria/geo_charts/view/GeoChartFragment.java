ab5b3ad23c0108f9fe866260d92ad2268d3c0120
nicobidone <nicobidone@gmail.com>
2019-06-18 00:28:26 -0300

    Minor fixes

--- app/src/main/java/com/unicen/exa/ingenieria/geo_charts/view/GeoChartFragment.java
+++ app/src/main/java/com/unicen/exa/ingenieria/geo_charts/view/GeoChartFragment.java
@@ -1,6 +1,5 @@
 package com.unicen.exa.ingenieria.geo_charts.view;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,32 +14,25 @@ public class GeoChartFragment extends Fragment {
 
     public static final String TAG = "GeoChartFragment";
     private static final String id_app  = "Android";
+    private static final String addr = "file:///android_asset/geocharts_region.html";
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_geo_chart, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_geo_chart, container, false);
         WebView myWebView = (WebView) view.findViewById(R.id.webview_geochart);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
-//        myWebView.getSettings().setSupportZoom(true);
-//        myWebView.getSettings().setBuiltInZoomControls(true);
-//        myWebView.getSettings().setDisplayZoomControls(false);
-//        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.getSettings().setSupportZoom(true);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(false);
+        myWebView.getSettings().setUseWideViewPort(true);
 
         myWebView.addJavascriptInterface(new WebAppInterface(getActivity()), id_app);
-        myWebView.loadUrl("file:///android_asset/geocharts_region.html");
+        myWebView.loadUrl(addr);
 
         return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-
     }
 }

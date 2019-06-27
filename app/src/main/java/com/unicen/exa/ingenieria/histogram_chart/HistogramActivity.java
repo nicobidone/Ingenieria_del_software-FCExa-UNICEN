4d0217a14a230d7f28074ae3d5ddb8e0f5c488b9
nicobidone <nicobidone@gmail.com>
2019-06-27 00:21:50 -0300

    updated barchart to 3.x version

--- app/src/main/java/com/unicen/exa/ingenieria/histogram_chart/HistogramActivity.java
+++ app/src/main/java/com/unicen/exa/ingenieria/histogram_chart/HistogramActivity.java
@@ -1,12 +1,13 @@
 package com.unicen.exa.ingenieria.histogram_chart;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -19,56 +20,105 @@ import com.unicen.exa.ingenieria.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class HistogramActivity extends AppCompatActivity {
+public class HistogramActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
+    private BarChart chart;
+    private SeekBar seekBar;
     private HashMap<String, Integer> result;
-    private BarChart barchart;
     private TextView country_selected;
     private TextView country_downloads;
 
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_histogram);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_barchart);
         country_selected = findViewById(R.id.country_selected);
         country_downloads = findViewById(R.id.country_downloads);
 
-
         result = (HashMap<String, Integer>) getIntent().getSerializableExtra("result");
 
-        barchart = findViewById(R.id.barchart);
-        barchart.setDrawBarShadow(false);
-        barchart.setDrawValueAboveBar(true);
-        //barchart.setMaxVisibleValueCount(50);
-        barchart.setPinchZoom(false);
-        barchart.setDrawGridBackground(true);
+        setTitle("HistogramActivity");
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        ArrayList<String> countries = new ArrayList();
+        seekBar = findViewById(R.id.seekBar1);
+        seekBar.setOnSeekBarChangeListener(this);
+
+        chart = findViewById(R.id.chart1);
+
+        chart.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        chart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+
+        chart.getAxisLeft().setDrawGridLines(false);
+
+        // setting data
+        seekBar.setProgress(10);
+
+        // add a nice and smooth animation
+        chart.animateY(1500);
+
+        chart.getLegend().setEnabled(false);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+        List<String> countries = new ArrayList<>();
+
         int i = 0;
         for(String key : result.keySet()){
-            barEntries.add(new BarEntry(result.get(key),i));
-            countries.add(key);
+            if (i < seekBar.getProgress()) {
+                values.add(new BarEntry(i, result.get(key)));
+                countries.add(key);
+            }
             i++;
         }
 
+        BarDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(values, "Data Set");
+            set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
+            set1.setDrawValues(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            chart.setData(data);
+            chart.setFitBars(true);
+        }
 
 
-        BarDataSet dataSet = new BarDataSet(barEntries, "Countries");
-        BarData data = new BarData(countries, dataSet);
+        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
 
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        barchart.setData(data);
-        barchart.getLegend().setEnabled(false);
-        barchart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
-            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-                country_selected.setText(countries.get(e.getXIndex()));
-
-                float a = e.getVal();
-                country_downloads.setText(String.valueOf(e.getVal()));
+            public void onValueSelected(Entry e, Highlight h) {
+                country_selected.setText(countries.get((int) e.getX()));
+                country_downloads.setText(String.valueOf((int)e.getY()));
             }
 
             @Override
@@ -76,6 +126,16 @@ public class HistogramActivity extends AppCompatActivity {
 
             }
         });
+        chart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
